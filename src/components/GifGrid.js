dbb41b69cd9218fc1723dfbbcd3765816ac2923e
Facundo <facundolobo2@gmail.com>
2021-01-14 13:20:51 -0300

    15. useFetchGifs - obtener imágenes y bandera de carga

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,21 +1,14 @@
 import React, { useState,useEffect } from 'react'
 import { useFetchGifs } from '../hooks/useFetchGifs'
-// import { getGifs } from '../helpers/getGifs';
-// import { GifGridItem } from './GifGridItem';
+ import { GifGridItem } from './GifGridItem';
 
 export const GifGrid = ({category}) => {
 
-// const [images, setImages] = useState([])
     
-    const {loading}= useFetchGifs();
+    const {data:images,loading}= useFetchGifs(category);
     
 
 
-
-//     useEffect(() => {
-//         getGifs(category)
-//         .then(setImages);
-//     },[category])
 
 
 
@@ -25,8 +18,9 @@ export const GifGrid = ({category}) => {
         <>
         <h3>{category}</h3>
 
-        {loading ? 'Cargando...' : 'Data cargada'}
-        {/* <div className="card-grid">
+        {loading && <p>Loading</p>}
+
+        <div className="card-grid">
             
             {
                 images.map(img => (
@@ -36,7 +30,7 @@ export const GifGrid = ({category}) => {
                 ) )
             }
             
-        </div> */}
+        </div>
         </>
     )
 }
--- src/hooks/useFetchGifs.js
+++ src/hooks/useFetchGifs.js
@@ -1,20 +1,34 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { getGifs } from "../helpers/getGifs";
 
 
  
-export const useFetchGifs = () => {
+export const useFetchGifs = (category) => {
    const [state, setstate] = useState({
        data: [],
        loading: true
    })
 
-   setTimeout(() => {
-    setstate({
-        data: [1,2,3,4,5,6,7],
-        loading: false
-    }) 
-   }, 3000);
    
+    useEffect(() => {
+        
+        getGifs(category)
+        .then(imgs => {
+
+            setTimeout(()=> {
+                console.log(imgs);
+                setstate({
+                   data: imgs,
+                   loading: false 
+                })
+                
+            },3000)
+        })
+
+
+    },[category])
+
+
    
    return state; //{data: [] , loading : true};
 }
